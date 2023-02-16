6742b7c885d0e4b023349b49fac2b98bcefed337
oceanexplains <121273516+oceanexplains@users.noreply.github.com>
2023-02-16 16:24:26 -0500

    Added API Models
    Added models for decoding structs from the API JSON result

---
+++ TrialrApp/API Models.swift
@@ -0,0 +1,62 @@
+//
+//  API Models.swift
+//  TrialrApp
+//
+//  Created by Ocean on 2/16/23.
+//
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let welcome = try? JSONDecoder().decode(Welcome.self, from: jsonData)
+
+import Foundation
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let studyFieldsResponse: StudyFieldsResponse
+
+    enum CodingKeys: String, CodingKey {
+        case studyFieldsResponse = "StudyFieldsResponse"
+    }
+}
+
+// MARK: - StudyFieldsResponse
+struct StudyFieldsResponse: Codable {
+    let apiVrs, dataVrs, expression: String
+    let nStudiesAvail, nStudiesFound, minRank, maxRank: Int
+    let nStudiesReturned: Int
+    let fieldList: [String]
+    let studyFields: [StudyField]
+
+    enum CodingKeys: String, CodingKey {
+        case apiVrs = "APIVrs"
+        case dataVrs = "DataVrs"
+        case expression = "Expression"
+        case nStudiesAvail = "NStudiesAvail"
+        case nStudiesFound = "NStudiesFound"
+        case minRank = "MinRank"
+        case maxRank = "MaxRank"
+        case nStudiesReturned = "NStudiesReturned"
+        case fieldList = "FieldList"
+        case studyFields = "StudyFields"
+    }
+}
+
+// MARK: - StudyField
+struct StudyField: Codable {
+    let rank: Int
+    let orgStudyID, briefTitle, condition, targetDuration: [String]
+    let centralContactName, centralContactPhone, centralContactEMail, locationFacility: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case rank = "Rank"
+        case orgStudyID = "OrgStudyId"
+        case briefTitle = "BriefTitle"
+        case condition = "Condition"
+        case targetDuration = "TargetDuration"
+        case centralContactName = "CentralContactName"
+        case centralContactPhone = "CentralContactPhone"
+        case centralContactEMail = "CentralContactEMail"
+        case locationFacility = "LocationFacility"
+    }
+}
---
+++ TrialrApp/TrialrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialrAppApp.swift
+//  TrialrApp
+//
+//  Created by Tomer Zilbershtein on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct TrialrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
