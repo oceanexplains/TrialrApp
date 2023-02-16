a53ff2e252bda7aadda76b2f611605426dbce2f0
oceanexplains <121273516+oceanexplains@users.noreply.github.com>
2023-02-16 16:36:28 -0500

    Created StudyRowItemView
    Created a view to be included in a study list view. It displays the brief title, condition, and facility of the study.

---
+++ TrialrApp/StudyRowItemView.swift
@@ -0,0 +1,36 @@
+//
+//  StudyRowItemView.swift
+//  TrialrApp
+//
+//  Created by Ocean on 2/16/23.
+//
+
+import SwiftUI
+
+struct StudyRowItemView: View {
+    let study: StudyField
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(study.briefTitle.first ?? "")
+                .font(.headline)
+                .lineLimit(1)
+            Text(study.condition.first ?? "")
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                .lineLimit(1)
+            Text(study.locationFacility.first ?? "")
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                .lineLimit(1)
+        }
+    }
+}
+
+//struct StudyRowItemView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        StudyRowItemView()
+//    }
+//}
+
+
